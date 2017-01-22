1e8dec30485b3a0e3807694ee8e63e91dc68a344
Gilgamech <git@Gilgamech.com>
2017-01-22 10:25:58 -0800

    Change background to fancy.

--- ARKMap.js
+++ ARKMap.js
@@ -11,7 +11,7 @@ var bgImage = new Image();
 bgImage.onload = function () {
   bgReady = true;
 };
-bgImage.src = "http://gilgamech.com/ark/TheCenter.jpg";
+bgImage.src = "http://gilgamech.com/ark/TheCenter_Pencil.jpg";
 
 // load clientClint
 var clientClintReady = false;
